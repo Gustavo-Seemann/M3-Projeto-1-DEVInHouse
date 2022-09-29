6f8a70797241cefa629d09e6bb36f47756d025c3
Luiz Gustavo Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-09-28 22:56:02 -0300

    FEAT: teste de pesquisa de get user de sucesso e sem reultados.

--- tests/users/test_list_users.py
+++ tests/users/test_list_users.py
@@ -14,3 +14,21 @@ def test_not_authorized_get_user(client):
     assert response.json['error'] == "Você não tem permissão"
 
     
+def test_sucess_get_user(client, logged_in_client):
+    headers = {
+        'Content-type': mimetype,
+        'Accept': mimetype
+    }
+    headers["Authorization"] = f"Bearer {logged_in_client}"
+    response = client.get("/user/?name=a", headers=headers)
+    assert response.status_code == 200
+    assert response.json['Status'] == "Sucesso"
+
+def test_not_found_get_user(client, logged_in_client):
+    headers = {
+        'Content-type': mimetype,
+        'Accept': mimetype
+    }
+    headers["Authorization"] = f"Bearer {logged_in_client}"
+    response = client.get("/user/?name=!", headers=headers)
+    assert response.status_code == 204
